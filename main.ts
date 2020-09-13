a0cb93f12802257017e8a7720b4d2837f503f642
Victor Shang <30793693+victorshang@users.noreply.github.com>
2020-09-13 23:00:34 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,16 @@
 input.onButtonPressed(Button.A, function () {
-    ESP8266.BigiotCheckServerTime()
+    ESP8266.BigiotUpdate2(
+    "12844",
+    "11251",
+    "123",
+    "11345",
+    "456"
+    )
+})
+input.onButtonPressed(Button.B, function () {
+    ESP8266.BigiotCheckServerDate(DateTimeFormat.DateTime)
     if (ESP8266.isLastCmdSuccessful()) {
-        basic.showString("" + (ESP8266.lastServerTime()))
+        basic.showString(ESP8266.lastServerTime())
     }
 })
 ESP8266.connectWifi(
@@ -12,32 +21,21 @@ BaudRate.BaudRate115200,
 "0987654323"
 )
 if (ESP8266.isWifiConnected()) {
-    basic.showNumber(0)
     ESP8266.connectToBigiotServer("www.bigiot.net", 8181)
     if (ESP8266.isBigiotConnected()) {
-        basic.showNumber(1)
         ESP8266.BigiotCheckout("12844", "fb0b41e34")
-        if (ESP8266.isLastCmdSuccessful()) {
-            basic.showString("a")
-        }
         ESP8266.BigiotCheckin("12844", "fb0b41e34")
         if (ESP8266.isLastCmdSuccessful()) {
-            basic.showString("b")
+            basic.showIcon(IconNames.Heart)
         }
     }
 }
 basic.forever(function () {
-    basic.pause(10000)
-    ESP8266.BigiotUpdate2(
-    "12844",
-    "11251",
-    convertToText(input.rotation(Rotation.Roll)),
-    "11345",
-    convertToText(input.rotation(Rotation.Pitch))
-    )
-})
-basic.forever(function () {
-    if (ESP8266.waitforCommand(1000)) {
+    if (ESP8266.waitforCommand(3000)) {
         basic.showString(ESP8266.lastCmd())
     }
+})
+basic.forever(function () {
+    ESP8266.BigiotBeat()
+    basic.pause(20000)
 })
